134eb33873706685fe357fae4aedbfbb58862815
Charles Liu <dongyangliu@dongyangs-MacBook-Air.local>
2014-08-19 16:49:12 -0400

    Issues with AFnetworking

--- iReporter/AFNetworking/API.m
+++ iReporter/AFNetworking/API.m
@@ -8,6 +8,39 @@
 
 #import "API.h"
 
+#define kAPIHost @"http://localhost"
+#define kAPIPath @"iReporter/"
 @implementation API
+
++ (API *)sharedInstance {
+    static API *sharedInstance = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        sharedInstance = [[API alloc] initWithBaseURL:[NSURL URLWithString: kAPIHost]];
+        //sharedInstance.securityPolicy = [AFSecurityPolicy policyWithPinningMode:AFSSLPinningModePublicKey];
+    });
+    return sharedInstance;
+}
+
+- (API *)init
+{
+    self = [super init];
+    
+    if (self) {
+        self.user = nil;
+        
+        self.requestSerializer = [AFJSONRequestSerializer serializer];
+        
+        [self set];
+        
+    }
+    
+    return self;
+}
+
+- (BOOL)isAuthorized {
+    return [[self.user objectForKey:@"IdUser"] intValue] > 0;
+}
+
 
 @end
